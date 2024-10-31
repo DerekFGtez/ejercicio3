818fbf00278a1315227fb3c070192731ab40bd85
DerekFGtez <fonseca_oliver376@comunidad.unam.mx>
2024-10-31 10:52:46 -0600

    First commit

---
+++ snake.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <termios.h>
+#define ANCHO 40
+#define ALTO 20
+#define ARRIBA 0
+#define ABAJO 1
+#define IZQUIERDA 2
+#define DERECHA 3
+#define MAX_LONGITUD[100]
+
+int tablero[20][40];
+struct serpiente
+{
+int serpiente[100]
+void direccion( )
+};
+ 
+typedef struct {
+    int x;
+    int y;
+} Punto;
+
+typedef struct {
+    Punto posicion[ANCHO*ALTO];
+    int longitud;
+    
+} Serpiente;
+
+void inicializarSerpiente(Serpiente *serpiente);
+
+
+
+//Variables globales del juego 
+; 
+int punto;
+int puntaje = 0;
+int posicionx = 1;
+int posiciony = 1;
+
+
+
